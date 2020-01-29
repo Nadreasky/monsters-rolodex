1218e6562477850c294d0b622f5d2eab166253e4
qhuynh <qhuynh@bocasay.com>
2020-01-29 20:09:57 +0700

    Updates

---
+++ precache-manifest.eaa9a517272db97a7fbd5ed03b33bcce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c37dfa5db130cec68fd04ee1d3a680d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "812c9a1f9424b79b1d27",
+    "url": "/monsters-rolodex/static/css/main.160e01bd.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "812c9a1f9424b79b1d27",
+    "url": "/monsters-rolodex/static/js/main.ef321c87.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
